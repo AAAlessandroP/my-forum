7d2a13eb443743b54ab060849614e23033bff3c9
Glitch (quilled-bactrosaurus) <none>
2019-12-27 19:46:20 +0000

    🌈🌞 Checkpoint ./server.js:9636903/3522

--- server.js
+++ server.js
@@ -5,18 +5,79 @@
 const express = require("express");
 const app = express();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const uri = `mongodb+srv://r00t:${process.env.PASS}@miocluster2-igwb8.mongodb.net/test?retryWrites=true&w=majority`;
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+var sessioni = {};
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.post("/login", (req, res) => {
+    var name = req.body.utente;
+    var pass = req.body.passw;
+
+    MongoClient.connect(uri, { useNewUrlParser: true }, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("trello");
+
+        dbo.collection("utenti").findOne({ Name: name }, function (err, resFind) {
+            if (err || resFind == null) {
+                res.sendStatus(401);
+                db.close();
+                return;
+            }
+            const hash = crypto.createHash("sha256");
+            hash.update(resFind.Salt + pass);
+            let hashed = hash.digest("base64");
+            if (resFind.HashedPwd === hashed) {
+                var sessId = crypto.randomBytes(32).toString("hex");
+                sessioni[sessId] = {
+                    IDUtente: resFind._id,
+                    Utente: resFind.Name,
+                    chiave: resFind.pass2cypher
+                };
+                // console.log(`sessioni dopo /login`, sessioni);
+                res.send(sessId);
+            } else res.sendStatus(401);
+            db.close();
+        });
+    });
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+
+app.post("/addUser", (req, res) => {
+    var name = req.body.utente;
+    var pass = req.body.passw;
+    var salt = crypto.randomBytes(32).toString("hex");
+    const hash = crypto.createHash("sha256");
+    hash.update(salt + pass);
+    let hashed = hash.digest("base64");
+
+    MongoClient.connect(uri, { useNewUrlParser: true }, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("trello");
+
+        var nuovo_utente = {
+            Name: name,
+            Salt: salt,
+            HashedPwd: hashed,
+            pass2cypher: pass
+        };
+        dbo.collection("utenti").insertOne(nuovo_utente, function (err, resIns) {
+            if (err) throw err;
+            console.log("1 nuovo utente inserito");
+            dbo.collection("utenti").findOne(nuovo_utente, function (err, resFind) {
+                if (err) {
+                    res.sendStatus(401);
+                    db.close();
+                    throw err;
+                }
+                var sessId = crypto.randomBytes(32).toString("hex");
+                sessioni[sessId] = {
+                    IDUtente: resFind._id,
+                    Utente: nuovo_utente.Name,
+                    chiave: nuovo_utente.pass2cypher
+                };
+                res.send(sessId);
+                db.close();
+            });
+        });
+    });
 });
