85abe260aebe3ed255c790bff952f7a2b792bcf9
Glitch (my-forum) <none>
2020-03-01 14:22:29 +0000

    🍘🐝 Checkpoint ./userPageMod.js:9636903/93 ./.glitch-assets:9636903/1380

--- userPageMod.js
+++ userPageMod.js
@@ -8,14 +8,13 @@ function page(uid) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Titolo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     
     </head>
     <body>
 
-        <img src="uploads/${uid}/pic" alt="profile pic" height="42" width="42">
-        
+        <img src="uploads/${uid}" alt="profile pic" height="42" width="42">
+
     </body>
     </html>`;
 };
