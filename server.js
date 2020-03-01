444495c31982e5f6fc0104518da990325f588521
Glitch (my-forum) <none>
2020-03-01 18:06:43 +0000

    🐊🏙 Checkpoint ./server.js:9636903/4

--- server.js
+++ server.js
@@ -234,7 +234,7 @@ app.post("/allThreads", async (req, res) => {
             .toArray();
         // console.log(dati)
         dati.forEach(async post => {
-            post.ByName = await db.db("forum").collection("utenti").findOne({ _id: ObjectId(post.By) }).project({ _id: 0, Name: 1 })
+            post.ByName = await db.db("forum").collection("utenti").findOne({/* _id: ObjectId(post.By) */}).project({ _id: 0, Name: 1 })
             return post
         })
         res.json(dati);
