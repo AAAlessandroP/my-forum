d940ebb9460bc73587c6e0757116532302d49c39
ale <ale@localhost.localdomain>
2020-05-23 12:25:04 +0200

    a

--- redis-stable/src/release.h
+++ redis-stable/src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "d5a909b6"
+#define REDIS_GIT_SHA1 "e07fefa4"
 #define REDIS_GIT_DIRTY "0"
-#define REDIS_BUILD_ID "localhost.localdomain-1589627621"
+#define REDIS_BUILD_ID "localhost.localdomain-1590228926"
